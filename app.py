aed74ef96ef150756621c1742575f073600f4a80
boandri <96402563+boandri@users.noreply.github.com>
2023-05-11 00:13:37 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+# name = 'hihi'
+# print(name[0:2])
+# print(123)
+# print(name[1:2])
+# #print(name * 100)
+# print("whoami")
+
+car = ['k5', 'white', 5000]
+car2 = {
+    'brand' : 'BMW', 
+    'model' : '520d'
+    }
+print(car)
+print(car2['brand'])
+car[1] = 'black'
+print(car)
+car.pop()
+print(car)
