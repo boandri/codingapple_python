2bb32292ba931851339a7fcbea01e355d74eac2d
boandri <96402563+boandri@users.noreply.github.com>
2023-05-24 17:03:34 -0400

    linkedin cover added

---
+++ SeoWooJang.py
@@ -0,0 +1,18 @@
+class I:
+    def __init__(my, name):
+        my.name = name
+
+class My(I):
+    Degree, Skills, Languages, Strength = {}, [], (), []
+
+my_instance = I('Seo Woo Jang')
+My.Degree = {'Centennial College': 'Software Engineering Technician', 'Ewha Womans University': 'Music Composition'}
+My.Skills = ['Programming', 'Web design', 'Data analysis', 'Computer networking', 'Operating system', 'SCM']
+My.Languages = ('Python', 'C#', 'Java', 'JavaScript', 'Node', 'MongoDB', 'Angular', 'React', 'Swift', 'Dart')
+My.Strength = ['Curiosity', 'Creativity', 'Detail-oriented', 'Positive attitude', 'Adaptability']
+
+print(my_instance.name)
+print(My.Degree)
+print(My.Skills)
+print(My.Languages)
+print(My.Strength)
